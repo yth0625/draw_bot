d24a9bae096686c0259f468f1b5eadfc50115d27
Taehyeon Yun <yth0625@gmail.com>
2019-02-14 17:47:46 +0900

    Change bot account authentication method.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Server which installed node
 
 # How To Use
 1. Setting Integration your [Mattermost Server](https://docs.mattermost.com/developer/slash-commands.html)
-2. run index.js file 
+2. Make bot [personal access token](https://docs.mattermost.com/developer/personal-access-tokens.html).
+3. Run index.js file.
 
 If you change configure run configure.js or change config.json file.
--- configure.js
+++ configure.js
@@ -19,38 +19,27 @@ module.exports = {
                 }
             });
 
-            rl.question('What is your Mattermost bot Id? ', (answer) => {
-                configFile.Mattermost_Bot_ID = answer;
+            rl.question('What is your Mattermost bot personal access token? ', (answer) => {
+                configFile.Mattermost_Bot_Personal_Token = answer;
                 fs.writeFile(filePath, JSON.stringify(configFile, null, '\t'), (err) => {
                     if (err) {
                         console.log(err);
                         process.exit(1);
                     }
-                });
 
-                rl.question('What is your Mattermost bot Password? ', (answer) => {
-                    configFile.Mattermost_Bot_Password = answer;
-                    fs.writeFile(filePath, JSON.stringify(configFile, null, '\t'), (err) => {
-                        if (err) {
-                            console.log(err);
-                            process.exit(1);
-                        }
-
-                        rl.question('Please enter the port to which you want to run this bot (default 8325, Port should be >= 0 and < 65536) ', (answer) => {
-                            answer === '' ? configFile.Use_Port = "8325" : configFile.Use_Port = answer;
-                            fs.writeFile(filePath, JSON.stringify(configFile, null, '\t'), (err) => {
-                                if (err) {
-                                    console.log(err);
-                                    process.exit(1);
-                                }
-                                rl.close();
-                                Run();
-                            });
+                    rl.question('Please enter the port to which you want to run this bot (default 8325, Port should be >= 0 and < 65536) ', (answer) => {
+                        answer === '' ? configFile.Use_Port = "8325" : configFile.Use_Port = answer;
+                        fs.writeFile(filePath, JSON.stringify(configFile, null, '\t'), (err) => {
+                            if (err) {
+                                console.log(err);
+                                process.exit(1);
+                            }
+                            rl.close();
+                            Run();
                         });
-
                     });
-                });
 
+                });
             });
         });
     }
