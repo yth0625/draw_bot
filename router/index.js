9c49155515b425f27c85bba61cd34b397d2330af
Taehyeon Yun <yth0625@gmail.com>
2019-02-19 14:56:44 +0900

    Added '/draw' path router

--- router/index.js
+++ router/index.js
@@ -20,9 +20,9 @@ module.exports = (app) => {
                             
                             memberFile.channelList.push({
                                 'channelId': channel_id,
-                                'memberList': [
+                                'memberList': 
                                     channelUserList.map( user => {return {"userName": user, "type": "O"}})
-                                ],
+                                ,
                                 'maxNumberToDraw': channelUserList.length >= 5 ? 5 : channelUserList.length 
                             });
         
@@ -36,15 +36,63 @@ module.exports = (app) => {
                 "title": "Draw Bot",
                 "text": "Click the button to try the Draw bot.",
                 "actions": [
-                    makeAction("Draw", '/draw'), 
-                    makeAction("Check Member", '/check'), 
-                    makeAction("Add Member", '/add'),
-                    makeAction("Delete Member", '/delete'),
-                    makeAction("Initialize Member", '/init')
+                    makeAction("Draw", 'draw'), 
+                    makeAction("Check Member", 'check'), 
+                    makeAction("Add Member", 'add'),
+                    makeAction("Delete Member", 'delete'),
+                    makeAction("Initialize Member", 'init')
                 ]
             }];
-        
             res.send({ response_type: 'in_channel', attachments });
+        }
+    });
+
+    app.post('/draw', (req, res) => {
+        const {channel_id} = req.body;
+
+        try {
+            const {number} = req.body.context;
+            memberFile.channelList.map(List => {
+                if (List.channelId === channel_id) {
+                    let outputMemberList = new Array();
+                    do {
+                        const randomNumber = ~~(Math.random() * List.memberList.length -1);
+
+                        if ( List.memberList[randomNumber].type === "O" &&
+                            outputMemberList.find( member => member.userName === List.memberList[randomNumber].userName) === undefined) {
+                            outputMemberList.push(List.memberList[randomNumber]);
+                        }
+
+                    } while (outputMemberList.length < number);
+
+                    const attachments = [{
+                        "title": "Draow Bot",
+                        "text": "Congratulations!!!" + outputMemberList.map((member) => {return ' @'+member.userName})
+                    }];
+    
+                    res.send({ update: { props: { attachments } } });
+                }
+            });
+        } catch (error) {
+            memberFile.channelList.map( channel => {
+                if ( channel.channelId === channel_id ) {
+                    let action = new Array();
+
+                    for(let a = 1; a <= channel.maxNumberToDraw; a++) { 
+                        action.push(makeAction(a + ' member', 'draw', {number: a}));
+                    }
+
+                    const attachments = [{
+                        "title": "Draow Bot",
+                        "text": "How many will you Draw?",
+                        "action": [
+                            action
+                        ]
+                    }];
+                    
+                    res.send({ update: { props: { attachments } } });
+                } 
+            });
         }
     });
 };
