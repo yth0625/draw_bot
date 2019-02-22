2dc56c53f22d78f1fbd736d7c1b34aac5ed64fdd
Taehyeon Yun <yth0625@gmail.com>
2019-02-22 10:40:22 +0900

    Separate setting section.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Server which installed node
 # How To Use
 1. Setting Integration your [Mattermost Server](https://docs.mattermost.com/developer/slash-commands.html)
 2. Make bot [personal access token](https://docs.mattermost.com/developer/personal-access-tokens.html)(This bot account should have `read_channel` permission for the channel. ).
-3. Run index.js file. (It is recommended to use the [forever](https://www.npmjs.com/package/forever) module.)
+3. Run configure.js file.
+4. Running bot using `npm start` command. 
 
 If you change configure run configure.js or change config.json file.
--- index.js
+++ index.js
@@ -4,24 +4,13 @@ const app = express();
 const bodyparser = require('body-parser');
 let configFile = require('./config/config.json');
 
-async function Init() {
-    if (configFile.Mattermost_Server_URL === '') {
-        const configure = require('./configure.js');
-        await configure.confiugre(Run);
-    } else 
-        Run().catch(error => {
-            console.log(error);
-        });
+if (configFile.Mattermost_Server_URL === '') {
+    console.log('You sholud run configure.js before start bot.');
+    process.exit(1);
 }
 
-async function Run() {
-    app.use(bodyparser.urlencoded({'extended':'true'}));
-    app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({'extended':'true'}));
+app.use(bodyparser.json());
 
-    require('./router')(app);
-    app.listen(configFile.Use_Port, () => console.log('app listening on port ' + configFile.Use_Port));
-}
-
-Init().catch(error => {
-    console.log(error);
-});
+require('./router')(app);
+app.listen(configFile.Use_Port, () => console.log('app listening on port ' + configFile.Use_Port));
