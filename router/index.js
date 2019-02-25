7ceb15baff070a2ddfb18ca04037a5914db8effb
Taehyeon Yun <yth0625@gmail.com>
2019-02-25 18:27:47 +0900

    when initalize memberlist delete existing memberlist

--- router/index.js
+++ router/index.js
@@ -22,6 +22,10 @@ function initialize(channel_id, text, res) {
                         .then( data => {
                             const channelUserList = data.map( user => { return user.username;} );
                             
+                            const deleteIndex = memberFile.channelList.findIndex( channel => channel.channelId === channel_id);
+                            if (deleteIndex > -1)
+                                memberFile.channelList.slice(deleteIndex, 1);
+
                             memberFile.channelList.push({
                                 'channelId': channel_id,
                                 'memberList': 
